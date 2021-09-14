51393c6758fc57b19b366af52bcbf1065af69bcf
quest <quest@mac.com>
2021-09-13 16:35:29 -1000

    Finish deposit screen

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,15 @@
 <template>
   <header class="bg-gray-900">
-    <div class="container flex flex-col flex-wrap p-5 md:items-center md:flex-row">
+    <div class="flex flex-col flex-wrap p-5 w-full">
       <nav>
         <ul class="flex flex-wrap items-center justify-center text-base">
-          <li class="mr-5 text-sm font-semibold hover:text-blue-500 text-white">
+          <li class="mx-3 text-sm font-semibold hover:text-blue-500 text-white">
             <router-link to="/">Wallet</router-link>
           </li>
-          <li class="mr-5 text-sm font-semibold hover:text-blue-500 text-white">
+          <li class="mx-3 text-sm font-semibold hover:text-blue-500 text-white">
             <router-link to="community">Community</router-link>
           </li>
-          <li class="mr-5 text-sm font-semibold hover:text-blue-500 text-white">
+          <li class="mx-3 text-sm font-semibold hover:text-blue-500 text-white">
             <router-link to="about">About</router-link>
           </li>
         </ul>
