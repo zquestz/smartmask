8571ee4112f83ac4cf87d50632a8c8c565238a03
quest <quest@mac.com>
2021-09-17 10:10:13 -1000

    Add a few more assets

--- src/assetList.js
+++ src/assetList.js
@@ -15,6 +15,167 @@ const assetList = {
     decimals: 2,
     balance: 0,
   },
+  "0x675e1d6fce8c7cc091aed06a68d079489450338a": {
+    symbol: "ARG",
+    name: "Bitcoin Cash Argentina",
+    type: "SEP20",
+    decimals: 18,
+    balance: 0,
+  },
+  "0x7b2b3c5308ab5b2a1d9a94d20d35ccdf61e05b72": {
+    symbol: "FLEXUSD",
+    name: "FlexUSD",
+    type: "SEP20",
+    decimals: 18,
+    balance: 0,
+  },
+  "0x98dd7ec28fb43b3c4c770ae532417015fa939dd3": {
+    symbol: "FLEX",
+    name: "FLEX Coin",
+    type: "SEP20",
+    decimals: 18,
+    balance: 0,
+  }
 };
 
 export { assetList };
+
+//   "CANDYMAN": {
+//     "address": "0x0D8b355f9CEDeB612f2df4B39CdD87059A244567",
+//     "image": "https://marketcap.cash/candyman.png",
+//     "liquidity_BCH": "1.1054",
+//     "liquidity_TOKEN": "79229.6",
+//     "name": "CandyMan",
+//     "symbol": "CANDYMAN",
+//     "websites": ["https://istmeprincess.github.io/CandyMan/"],
+//     "telegram": "https://t.me/CandyManToken"
+//   },
+//   "HAM": {
+//     "address": "0x252fd94f3Fb53D3D62F4FEc708501ACd59A57e52",
+//     "image": "https://marketcap.cash/ham.png",
+//     "name": "HAM Token",
+//     "symbol": "HAM"
+//   },
+//   "HODL": {
+//     "address": "0xB24D7763516bca9656779d760be9a32490f46E27",
+//     "image": "https://marketcap.cash/hodl.png",
+//     "name": "HODL",
+//     "symbol": "HODL"
+//   },
+//   "HONK": {
+//     "address": "0xF2d4D9c65C2d1080ac9e1895F6a32045741831Cd",
+//     "image": "https://marketcap.cash/honk.png",
+//     "name": "Honk",
+//     "symbol": "HONK",
+//     "websites": ["https://honkhonk.io/"]
+//   },
+//   "KITTEN": {
+//     "address": "0x7ebeadb95724a006afaf2f1f051b13f4ebebf711",
+//     "image": "https://marketcap.cash/kitten.png",
+//     "name": "CashKitten",
+//     "symbol": "KITTEN",
+//     "twitter": "https://twitter.com/CashKitten_BCH",
+//     "telegram": "https://t.me/CashKitten_BCH"
+//   },
+//   "KNUTH": {
+//     "address": "0xc70c7718C7f1CCd906534C2c4a76914173EC2c44",
+//     "image": "https://marketcap.cash/knuth.png",
+//     "name": "Knuth",
+//     "symbol": "KNUTH",
+//     "websites": ["https://kth.cash/"]
+//   },
+//   "LIGHTNING": {
+//     "address": "0xc2100c9486B320B6db2Fc6D1f2BeAAB713fF069D",
+//     "image": "https://marketcap.cash/lightning.png",
+//     "name": "Lightning",
+//     "symbol": "LIGHTNING"
+//   },
+//   "MAZE": {
+//     "address": "0x481de06dca0198844faa36fca04db364e5c2f86c",
+//     "image": "https://marketcap.cash/maze.png",
+//     "name": "MAZE",
+//     "symbol": "MAZE",
+//     "websites": ["https://mazetoken.github.io"]
+//   },
+//   "MCASH": {
+//     "address": "0x778C1c04D68c74FfdECBc0950d51033699c8Eb0B",
+//     "image": "https://marketcap.cash/monkeycash.jpeg",
+//     "name": "Monkey Cash",
+//     "symbol": "MCASH",
+//     "telegram": "https://t.me/imonkeycash",
+//     "websites": ["https://imonkey.cash/"]
+//   },
+//   "MGOT": {
+//     "address": "0x7DF65F158126898725f262378538B60db543C11A",
+//     "image": "https://marketcap.cash/mgot.png",
+//     "name": "MGOT",
+//     "symbol": "MGOT"
+//   },
+//   "ORB": {
+//     "address": "0xff3ed63bf8bc9303ea0a7e1215ba2f82d569799e",
+//     "image": "https://marketcap.cash/orb.png",
+//     "name": "ORB",
+//     "symbol": "ORB",
+//     "websites": ["https://enter-the-sphere.com/"]
+//   },
+//   "PHA": {
+//     "address": "0x5a3bb59f34d60e9eb5643fb80c8d712275f6a96a",
+//     "image": "https://marketcap.cash/alpha.png",
+//     "name": "Alpha",
+//     "symbol": "PHA"
+//   },
+//   "SHIBBCH": {
+//     "address": "0x2f309b9d47b1ce7f0ec30a26bab2deab8c4ea5e9",
+//     "image": "https://marketcap.cash/shibbch.png",
+//     "name": "Shiba BCH",
+//     "symbol": "SHIBBCH",
+//     "telegram": "https://t.me/ShibaCashBCH"
+//   },
+//   "SPICE": {
+//     "address": "0xe11829a7d5d8806bb36e118461a1012588fafd89",
+//     "image": "https://marketcap.cash/spice.png",
+//     "name": "SPICE",
+//     "symbol": "SPICE",
+//     "websites": ["https://spicetoken.org/"]
+//   },
+//   "UATX": {
+//     "address": "0xffa2394b61d3de16538a2bbf3491297cc5a7c79a",
+//     "image": "https://marketcap.cash/uatx.jpg",
+//     "name": "UatX Token",
+//     "symbol": "UATX",
+//     "websites": ["https://uat.cash"]
+//   },
+//   "VANDALS": {
+//     "address": "0x2a886b947d5a8f0e09408c9069914fa44c06ed7e",
+//     "image": "https://marketcap.cash/vandals.png",
+//     "name": "Vandals",
+//     "symbol": "VANDALS",
+//     "websites": ["https://vandalstoken.onuniverse.com/"]
+//   },
+//   "WRS": {
+//     "address": "0x445B3712A09f8102Dd0c1ffb6B3b0dE4D3B643b7",
+//     "image": "https://marketcap.cash/thewirelesstoken.png",
+//     "name": "The Wireless Token",
+//     "symbol": "WRS"
+//   },
+//   "ZOMBIE": {
+//     "address": "0x80453acdfe0073d6743b27d72e06f48777eead80",
+//     "image": "https://marketcap.cash/zombie.png",
+//     "name": "Zombie",
+//     "symbol": "ZOMBIE",
+//     "websites": ["https://zombies.onuniverse.com"]
+//   },
+//   "sBUSD": {
+//     "address": "0x9288df32951386A8254aEaF80a66B78cCaf75b82",
+//     "image": "https://marketcap.cash/sbusd.jpg",
+//     "name": "Smart BUSD",
+//     "symbol": "sBUSD"
+//   },
+//   "sVNT": {
+//     "address": "0x8f66C730e383Bb412a9a3cB58178BAb2f432d194",
+//     "image": "https://marketcap.cash/svnt.png",
+//     "name": "sVNT",
+//     "symbol": "sVNT",
+//     "websites": ["https://svntfarm.netlify.app/farms"]
+//   }
+// }
